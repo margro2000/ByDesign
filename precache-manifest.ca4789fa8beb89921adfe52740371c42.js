3e01d727f3afdc5c33a5961f67f27ff7607894ea
Margarita Groisman <mg@Margaritas-MacBook-Pro.local>
2020-10-31 20:19:49 -0400

    Updates

---
+++ precache-manifest.ca4789fa8beb89921adfe52740371c42.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b028f29862eb5cdff6bdd00ef8a1a368",
+    "url": "/ByDesign/index.html"
+  },
+  {
+    "revision": "59b7a38c4ca75119e2bf",
+    "url": "/ByDesign/static/css/main.93f627b5.chunk.css"
+  },
+  {
+    "revision": "281443d9511a21782cdc",
+    "url": "/ByDesign/static/js/2.e10fab4e.chunk.js"
+  },
+  {
+    "revision": "196dfe13b803303c49673bff5ba2ea74",
+    "url": "/ByDesign/static/js/2.e10fab4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b7a38c4ca75119e2bf",
+    "url": "/ByDesign/static/js/main.6757da98.chunk.js"
+  },
+  {
+    "revision": "ed6e6c10c4bc878885ab",
+    "url": "/ByDesign/static/js/runtime-main.058170b1.js"
+  },
+  {
+    "revision": "8fb94b00e2937576a56266d0a8938469",
+    "url": "/ByDesign/static/media/cartoon_img_boy.8fb94b00.png"
+  },
+  {
+    "revision": "02a0e1fb891b8c26fb4555ed055e156a",
+    "url": "/ByDesign/static/media/cat_face.02a0e1fb.png"
+  },
+  {
+    "revision": "83626c97f4e4afea8e1e3ca876d03347",
+    "url": "/ByDesign/static/media/frog_face.83626c97.png"
+  },
+  {
+    "revision": "9f89e266de9a6dc5a61391cd6b0c0c62",
+    "url": "/ByDesign/static/media/giraffe_face.9f89e266.png"
+  },
+  {
+    "revision": "f4795b399934dc52c3b6e8b9d6bb8fc2",
+    "url": "/ByDesign/static/media/whiteDotV3.f4795b39.png"
+  }
+]);
